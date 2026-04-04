43d1870fe06ff32db7d349455379de26ae717560
Norsico <2542313773@qq.com>
2026-04-04 14:16:26 +0800

    修正 app_line 头文件对 IPS_ENABLE 的引用
    - 在 app_line.h 中补充 system_state.h，确保头文件内可直接拿到 IPS_ENABLE 定义。
    
    - 按 1062586 的修复方式恢复 line_app_render_frame 的条件编译判断。

--- project/user/app/app_line.h
+++ project/user/app/app_line.h
@@ -2,6 +2,7 @@
 #define _APP_LINE_H_
 
 #include "dev_flash.h"
+#include "system_state.h"
 #include "zf_common_typedef.h"
 
 typedef enum
@@ -20,7 +21,7 @@ void line_app_get_tune_range(line_tune_slot_t slot, uint16 *min_value, uint16 *m
 uint16 line_app_get_tune_value(line_tune_slot_t slot);
 uint8 line_app_set_tune_value(line_tune_slot_t slot, uint16 value);
 uint8 line_app_save_tune_page(void);
-#if defined(IPS_ENABLE) && IPS_ENABLE
+#if IPS_ENABLE
 void line_app_render_frame(void);
 #endif
 
