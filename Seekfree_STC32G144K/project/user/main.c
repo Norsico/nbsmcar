a22a22b3c2810b9837038bc9fcfa049426c98cba
Norsico <2542313773@qq.com>
2026-05-01 23:37:10 +0800

    更新注释

--- Seekfree_STC32G144K/project/user/main.c
+++ Seekfree_STC32G144K/project/user/main.c
@@ -6,8 +6,11 @@ void main(void)
     clock_init(SYSTEM_CLOCK_96M);            // 时钟配置及系统初始化<务必保留>
     debug_init();                            // 调试串口信息初始化
 
+    /********** 模块初始化 *********/
     motor_init();
-    motor_set_target(0, 0);                  // 上电停轮
+
+    /********** 参数初始化 **********/
+    motor_set_target(0, 0);                  // 停止后轮
 
     while(1)
     {
